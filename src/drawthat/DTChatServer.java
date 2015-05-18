70456ba2508f3c12ff7a570a2b368e56225472e1
Christopher Adams <christopheradams1995@hotmail.com>
2015-05-18 22:02:22 +1000

    Cleaned up the code
    -Fixed up all text formatting issues
    -Added all new comments
    -Fixed some too closely coupled problems

--- src/drawthat/DTChatServer.java
+++ src/drawthat/DTChatServer.java
@@ -1,20 +1,26 @@
+/**
+ * DTChatServer
+ * 
+ * v1.0
+ * 
+ * 15/05/2015
+ * 
+ * This file is owned by Christopher Adams
+ */
 
 package drawthat;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.ListIterator;
 import java.util.Random;
 import java.util.Scanner;
 
-public class DTChatServer implements Runnable
-{
+//Deals with all the server side game logic and client logic
+public class DTChatServer implements Runnable {
     private boolean isEnd = false;
     public int maxplayers = 8;
     static ArrayList clientsInput = new ArrayList();
@@ -35,24 +41,20 @@ public class DTChatServer implements Runnable
     //packs of words to add to the game
     boolean easyPack , medPack, hardPack;
     
-    public DTChatServer(boolean easyPack ,boolean medPack ,boolean hardPack , int maxPlayers)
-    {
+    //Starts a new server depending on what settings were given
+    public DTChatServer(boolean easyPack ,boolean medPack ,boolean hardPack , int maxPlayers) {
         this.maxplayers = maxPlayers;
         this.easyPack = easyPack;
         this.medPack = medPack;
         this.hardPack = hardPack;
-        
-        
         
         scores  = new int[maxplayers];
         players = new String[maxplayers];
         playerTurn = players[numTurn];
         
         loadPacks(easyPack , medPack , hardPack);
-        //System.out.println(words.toString());
         
-        for(int i=0;i<maxplayers;i++)
-        {
+        for(int i=0;i<maxplayers;i++) {
             scores[i] = 0;
         }
 
@@ -60,76 +62,65 @@ public class DTChatServer implements Runnable
         t.start();
     }
     
-    public void addClient(DataInputStream in, DataOutputStream out)
-    {
+    //Adds the client streams and gives the user their ID
+    public void addClient(DataInputStream in, DataOutputStream out) {
         itIn.add(in);
         itOut.add(out);
-        try
-        {
+        try {
             out.writeUTF("[OP_ID]" + (numPlayers+1));//gives the player their ID
             System.out.println("ID = " + (numPlayers+1));
-            //sends the scoreboard over
-            //updateScores();
             
-        }catch(Exception er){Game.logMessage(er.getMessage());}
-        //System.out.println("Client has been added to the arrayList");
+        }
+        catch(Exception er) {
+            DrawThat.logMessage(er.getMessage());
+        }
     }
     
-    public void loadPacks(boolean easy, boolean med , boolean hard)
-    {
+    //Reads the word files and saves them
+    public void loadPacks(boolean easy, boolean med , boolean hard) {
         Scanner scan = null;
         File easyFile = new File("words/easyWords.txt");
         File medFile = new File("words/mediumWords.txt");
         File hardFile = new File("words/hardWords.txt");
         
-        try
-        {
-            if(easy)
-            {
+        try {
+            if(easy) {
                 scan = new Scanner(easyFile);
-                while(scan.hasNextLine())
-                {
+                while(scan.hasNextLine()) {
                     String s = scan.nextLine();
-                    if(!s.equals("") && !s.equals(" "))
-                    {
+                    if(!s.equals("") && !s.equals(" ")) {
                         words.add(s);
                     }
                 }
             }
-            if(med)
-            {
+            if(med) {
                 scan = new Scanner(medFile);
-                while(scan.hasNextLine())
-                {
+                while(scan.hasNextLine()) {
                     String s = scan.nextLine();
-                    if(!s.equals("") && !s.equals(" "))
-                    {
+                    if(!s.equals("") && !s.equals(" ")) {
                         words.add(s);
                     }
                 }
             }
-            if(hard)
-            {
+            if(hard) {
                 scan = new Scanner(hardFile);
-                while(scan.hasNextLine())
-                {
+                while(scan.hasNextLine()) {
                     String s = scan.nextLine();
-                    if(!s.equals("") && !s.equals(" "))
-                    {
+                    if(!s.equals("") && !s.equals(" ")) {
                         words.add(s);
                     }
                 }
             }
-        }catch(Exception er)
+        }
+        catch(Exception er)
         {
             er.printStackTrace();
-            Game.logMessage(er.getMessage());
+            DrawThat.logMessage(er.getMessage());
         }
         nextWord = ran.nextInt(words.size());
     }
     
-    public void startNewGame()// not used yet
-    {
+    public void startNewGame() {
         itOut = clientsOutput.listIterator();
         while(itOut.hasNext())
         {
@@ -145,44 +136,38 @@ public class DTChatServer implements Runnable
                 out.flush();
             }catch(Exception er)
             {
-                Game.logMessage(er.getMessage());
+                DrawThat.logMessage(er.getMessage());
             }
         }
     }
     
-    public void updateTurn()
-    {
+    //Changes whos turn it is then updates the player that it's their turn
+    public void updateTurn() {
         playerTurn = players[numTurn];
         updatePlayer("[OP_TURN]"+playerTurn + "," + words.get(nextWord));
-        
     }
     
-    /**
-     * Give this method an OP message to send to all players
-     */
-    public void updatePlayer(String message)
-    {
+    //Sends a message to all players
+    public void updatePlayer(String message) {
         itOut = clientsOutput.listIterator();
-        while(itOut.hasNext())
-        {
-            try
-            {
+        while(itOut.hasNext()) {
+            try {
                 DataOutputStream out = (DataOutputStream)itOut.next();
-                //System.out.println("sent message from server: " + message);
-                    //out.writeUTF("[OP_SCORE]"+players[i] +","+scores[i]);
-                    out.writeUTF(message);
+                out.writeUTF(message);
             
                 out.flush();
-            }catch(Exception er)
-            {
-                Game.logMessage(er.getMessage());
+            }
+            catch(Exception er) {
+                DrawThat.logMessage(er.getMessage());
                 er.printStackTrace();
             }
         }
     }
     
-    public void run()
-    {
+    //Checks to see if a message was recieved from a client. Decodes the message
+    // and responds if it was for the server. Broadcasts the message to all the 
+    // client
+    public void run() {
 
         String message = "";
         boolean newMessage = false;
@@ -190,25 +175,20 @@ public class DTChatServer implements Runnable
         //for the index of the current person.
         int cur = 1;
         
-        while(!isEnd)
-        {
+        while(!isEnd) {
             i++;
             
-            try
-            {
+            try {
                 itIn = clientsInput.listIterator();
                 itOut = clientsOutput.listIterator();
                 
-                
-                while(itIn.hasNext())
-                {
+                while(itIn.hasNext()) {
                     // checks the input from each client that's connected to the server
                     DataInputStream in = (DataInputStream)itIn.next();
                     cur++;
                     int count = in.available();
                     
-                    if(count > 0)
-                    {
+                    if(count > 0) {
                         message = in.readUTF(); 
                         newMessage = true;
                         
@@ -217,8 +197,7 @@ public class DTChatServer implements Runnable
                         int index = message.indexOf("]"); //gets the index of the end of the OP code
                         String Op = message.substring(0, index+1); // used for sending different messages
                     
-                        if(Op.equals("[OP_newPlayer]"))
-                        {
+                        if(Op.equals("[OP_newPlayer]")) {
                             String name = message.substring(index+1);
                             System.out.println(name);
                             name = getNewName(name);
@@ -230,12 +209,11 @@ public class DTChatServer implements Runnable
                             numPlayers++;
                         }
                         // checks if the player correctly guessed the word
-                        if(Op.equals("[OP_MESSAGE]"))//chat message
-                        {
+                        if(Op.equals("[OP_MESSAGE]")){ //chat message
+                        
                             String text = message.substring(index+1).toLowerCase();
                             String currentWord = (String)words.get(nextWord);
-                            if(text.contains(currentWord.toLowerCase()))
-                            {
+                            if(text.contains(currentWord.toLowerCase())) {
                                 message = "[OP_PlayerMessage]"+cur+ ",correctGuess";
                                 scores[cur-1] += 1;
                                 scores[numTurn] += 2;
@@ -243,36 +221,24 @@ public class DTChatServer implements Runnable
                                 numTurn++;
                                 if(numTurn >= this.numPlayers)
                                     numTurn = 0;
-                                
                             }
                         }
                         //skips the current word and gives the next
-                        if(Op.equals("[OP_SKIP]"))
-                        {
+                        if(Op.equals("[OP_SKIP]")) {
                             nextWord = ran.nextInt(words.size());
                         }
-                        
                     }
                     
-
-                    
                     //if a message is received from one of the users then it's broadcasted to everyone.
-                    while(itOut.hasNext() && newMessage)
-                    {
-                            
+                    while(itOut.hasNext() && newMessage) {
                         DataOutputStream out = (DataOutputStream)itOut.next();
                         //System.out.println("sent message from server: " + message);
                         out.writeUTF(message);
                         out.flush();
-                        
                     }
                     
-                    if(i>100 && this.numPlayers >= 2)
-                    {
-                        
-                        for(int j=0;j<numPlayers;j++)
-                        {
-                            //out.writeUTF("[OP_SCORE]"+players[i] +","+scores[i]);
+                    if(i>100 && this.numPlayers >= 2) {
+                        for(int j=0;j<numPlayers;j++) {
                             updatePlayer("[OP_SCORE]"+players[j] +","+scores[j]);
                         }
                         
@@ -283,22 +249,17 @@ public class DTChatServer implements Runnable
                     newMessage = false;
                 }
                 Thread.sleep(5);
-                
-                            
-                
             }
-            catch(SocketTimeoutException s)
-            {
-                try
-                {
-                    Game.logMessage(s.getMessage());
-                }catch(Exception er){er.printStackTrace();}
-                //break;
+            catch(SocketTimeoutException s) {
+                try {
+                    DrawThat.logMessage(s.getMessage());
+                }catch(Exception er) {
+                    er.printStackTrace();
+                }
             }
-            catch(Exception e)
-            {
+            catch(Exception e) {
                 e.printStackTrace();
-                Game.logMessage(e.getMessage());
+                DrawThat.logMessage(e.getMessage());
                 break;
             }
             cur = 0;
@@ -307,21 +268,16 @@ public class DTChatServer implements Runnable
     
     //checks to see if a player's name already exists. If it does then it adds a number to the end.
     // returns the original name if it doesn't exist or returns the new name if it does exist with the number appended
-    public String getNewName(String name)
-    {
+    public String getNewName(String name) {
         String newname = name;
         int num = 0;
         
-        for(int i=0;i<this.numPlayers;i++)
-        {
-            if(players[i].equals(newname))
-            {
+        for(int i=0;i<this.numPlayers;i++) {
+            if(players[i].equals(newname)) {
                 newname = name + num++;
                 i = 0;
             }
         }
-        
         return newname;
-    }
-            
+    }  
 }
